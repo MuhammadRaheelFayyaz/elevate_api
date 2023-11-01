6a80f9f87eff8e56bf620b6e5904eff8d221020c
moin105 <moinlatif105@gmail.com>
2023-11-01 20:31:29 +0500

    api - hub page done

---
+++ pages/api-hub/index.js
@@ -0,0 +1,32 @@
+import ApiHubLayout from "@/components/ApiHubLayout/ApiHubLayout";
+import ApiHubCard from "@/components/Crad/ApiHubCard";
+import RecommendApiCard from "@/components/Crad/RecommendApiCard";
+import Navbar from "@/components/Navbar/Navbar";
+import Typography from "@/components/Typegraphy/Typography";
+import React from "react";
+import Slider from "react-slick";
+
+export default function ApiHub() {
+  return (
+    <div>
+      <ApiHubLayout>
+        {/* <Slider> */}
+        <div className="flex  justify-center gap-3	py-8">
+          <ApiHubCard image="https://picsum.photos/500/300" heading="Api 1" />
+          <ApiHubCard image="https://picsum.photos/500/300" heading="Api 2" />
+          <ApiHubCard image="https://picsum.photos/500/300" heading="Api 3" />
+          <ApiHubCard image="https://picsum.photos/500/300" heading="Api 4" />
+        </div>
+        <Typography variant="h2" text="Recommended APIs" className="text-left font-bold" />
+        <Typography variant="body" text="Discover and connect to thousands of APIs" className="text-left" />
+        <div className="flex  justify-center gap-3	py-8">
+          <RecommendApiCard heading="TEST TEASED"text="EEEEEEEEEEEEEEEEE EEEEEEEEEEEEEEEEEE EEEDDDDDDDDDD"time="23"tick="02"trend="30"/>
+          <RecommendApiCard heading="TEST TEASED"text="EEEEEEEEEEEEEEEEE EEEEEEEEEEEEEEEEEE EEEDDDDDDDDDD"time="23"tick="02"trend="30"/>
+          <RecommendApiCard heading="TEST TEASED"text="EEEEEEEEEEEEEEEEE EEEEEEEEEEEEEEEEEE EEEDDDDDDDDDD"time="23"tick="02"trend="30"/>
+          <RecommendApiCard heading="TEST TEASED"text="EEEEEEEEEEEEEEEEE EEEEEEEEEEEEEEEEEE EEEDDDDDDDDDD"time="23"tick="02"trend="30"/>
+        </div>
+        {/* </Slider> */}
+      </ApiHubLayout>
+    </div>
+  );
+}
