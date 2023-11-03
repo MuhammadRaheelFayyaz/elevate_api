fe0a55377f23bb050ff6f7ac38856e8cbc70efc9
Muhammad Raheel <rahelfayyazid@gmail.com>
2023-11-03 21:19:00 +0500

    Add dashboard page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 import ApiPageLayout from "@/components/ApiHubLayout/ApiHubLayout";
+import {useRouter} from 'next/router';
 
 export default function Home() {
-
-    return (
-        <ApiPageLayout>
-            <div>Children</div>
-        </ApiPageLayout>
-    )
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/api-hub')
+  }, [])
+  return (
+    null
+  )
 }
